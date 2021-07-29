7b7cc306cf419a29e3284a829110fa811beb5a0e
gabriel.casagrande <gabrielcasagrande.zetta@ufla.br>
2021-07-28 21:00:13 -0300

    🎉 project startup with typescript, ts-node-dev, express and linter

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "singleQuote": true,
+  "trailingComma": "es5",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": true,
+  "semi": true,
+  "tabWidth": 2,
+  "useTabs": false,
+  "endOfLine": "lf",
+}
